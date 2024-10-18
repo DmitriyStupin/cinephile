92d92a430553cf3d7df1b371df4ca90dcaa44fce
Dmitry Stupin <dm.stup@mail.ru>
2024-10-18 15:36:12 +0700

    Доработка компонентов и отрисовка на странице

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import HeaderProfileView from './view/header-profile-view.js';
+import FooterStatisticsView from './view/footer-statistics-view.js';
+import FilterView from './view/filter-view.js';
+
+import FilmsPresenter from './presenter/films-presenter.js';
+
 import {render} from './render.js';
 
 const siteHeaderElement = document.querySelector('.header');
+const siteFooterElement = document.querySelector('.footer__statistics');
+const siteMainElement = document.querySelector('.main');
+
+const filmsPresenter = new FilmsPresenter();
 
 render(new HeaderProfileView(), siteHeaderElement);
+render(new FooterStatisticsView(), siteFooterElement);
+render(new FilterView(), siteMainElement);
+
+filmsPresenter.init(siteMainElement);
